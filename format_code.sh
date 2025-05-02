01b375457a3a599da459310a39e1ea92b5e6dd59
efournierrobert <elliott1447@gmail.com>
2025-05-02 16:00:20 -0400

    Formating script

---
+++ format_code.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+go fmt github.com/eFournierRobert/themedia/cmd/themedia
+go fmt github.com/eFournierRobert/themedia/internal/handlers
+go fmt github.com/eFournierRobert/themedia/internal/middleware
+go fmt github.com/eFournierRobert/themedia/internal/models
+go fmt github.com/eFournierRobert/themedia/internal/tools
+go fmt github.com/eFournierRobert/themedia/internal/handlers
