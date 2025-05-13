fd8a90b96e3b069ec5cd78df525b46eef846fc2b
efournierrobert <elliott1447@gmail.com>
2025-05-13 13:02:17 -0400

    Updated format_code

--- format_code.sh
+++ format_code.sh
@@ -6,3 +6,4 @@ go fmt github.com/eFournierRobert/themedia/internal/middleware
 go fmt github.com/eFournierRobert/themedia/internal/models
 go fmt github.com/eFournierRobert/themedia/internal/tools
 go fmt github.com/eFournierRobert/themedia/internal/handlers
+go fmt github.com/eFournierRobert/themedia/test/tools
